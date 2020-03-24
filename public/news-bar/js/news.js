b5d1db92ffc53f0c202ead516b518238d07fad81
ManoRanjana De Silva <manoranjanad@embla.asia>
2020-03-24 17:55:47 +0530

    news update

--- public/news-bar/js/news.js
+++ public/news-bar/js/news.js
@@ -328,7 +328,7 @@
     autostart: true,
     property: "value",
     onComplete: null,
-    duration: 300000,
+    duration: 450000,
     padding: 10,
     marquee_class: ".marquee",
     container_class: ".simple-marquee-container",
