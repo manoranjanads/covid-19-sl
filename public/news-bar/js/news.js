9b4bc5eb33accbbcb776719094a7f35aaa3b6eff
ManoRanjana De Silva <manoranjanad@ieee.org>
2020-03-30 07:22:10 +0530

    news update

--- public/news-bar/js/news.js
+++ public/news-bar/js/news.js
@@ -328,7 +328,7 @@
     autostart: true,
     property: "value",
     onComplete: null,
-    duration: 550000,
+    duration: 640000,
     padding: 10,
     marquee_class: ".marquee",
     container_class: ".simple-marquee-container",
