9ed49e5caf53dc2cf9b3a5357890918d7a040bac
ManoRanjana De Silva <manoranjana@ManoRanjanas-MacBook-Pro.local>
2020-10-14 22:40:18 +0530

    latest update

--- public/news-bar/js/news.js
+++ public/news-bar/js/news.js
@@ -328,7 +328,7 @@
     autostart: true,
     property: "value",
     onComplete: null,
-    duration: 200200,
+    duration: 200800,
     padding: 10,
     marquee_class: ".marquee",
     container_class: ".simple-marquee-container",
